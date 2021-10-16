92da3c60a29cea925adb9a99575b29484551bd04
SwaroopThota <swaroopthota2001@gmail.com>
2021-10-16 12:08:40 +0530

    edit and Delete functionality for posts

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/firstdb")
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(expressLayouts);
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("layout", "./layouts/layout");
 
@@ -28,23 +29,42 @@ app.get("/about", (req, res) => {
   res.render("about", { pageTitle: "The Daily Journal | About" });
 });
 app.get("/compose", (req, res) => {
-  res.render("compose", { pageTitle: "The Daily Journal | Compose" });
+  res.render("compose", { pageTitle: "The Daily Journal | Compose", postEndpoint: "/compose" });
 });
 app.get("/posts",async (req, res) => {
   const posts = await blogModel.find().sort({'_id':-1});
   res.render("posts", { posts: posts, pageTitle: "The Daily Journal | Posts" });
 });
-app.get("/posts/:title",async (req, res) => {
-  try {
-    let post = await blogModel.findOne({postLinkTitle: req.params.title});
+app.get("/posts/:title",(req, res) => {
+    let post = req.post;
     res.render("post", {
       post: post,
       pageTitle: "The Daily Journal | Posts | " + post.title,
     });
-  } catch (e) {
-    res.status(404).render('error404',{pageTitle: "Page Not Found....!"})
+});
+app.delete("/posts/:title", async (req, res) => {
+  let post = req.post;
+  try{
+    await post.remove();
+    res.sendStatus(200);
+  }catch(e){
+    res.sendStatus(500);
   }
 });
+app.get("/posts/:title/edit",(req, res) =>{
+    let post = req.post;
+    res.render("compose", {
+      desc: post.desc,
+      pageTitle: "The Daily Journal | Post Edit | " + post.title,
+      postEndpoint: `/posts/${post.postLinkTitle}/edit`,
+    });
+})
+app.post("/posts/:title/edit", async (req, res) =>{
+    let post = req.post;
+    post.desc = req.body.desc;
+    await post.save();
+    res.redirect(`/posts/${post.postLinkTitle}`);
+})
 app.post("/compose",async (req, res) => {
   const post = new blogModel({
     title: req.body.title.trim(),
@@ -55,6 +75,14 @@ app.post("/compose",async (req, res) => {
   post.save();
   res.redirect("/");
 });
+app.param("title", async (req, res,next,title) => {
+  try {
+    req.post = await blogModel.findOne({postLinkTitle: req.params.title});
+    next();
+  } catch (e) {
+      res.status(404).render('error404',{pageTitle: "Page Not Found....!"})
+    }
+})
 app.get('*', function(req, res){
   res.status(404).render('error404',{pageTitle: "Page Not Found....!"})
 });
